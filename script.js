9e5bef0b9854e2de062f39aad80e8b7e3df6ba1e
Trupti100 <127935389+Trupti100@users.noreply.github.com>
2023-09-08 14:13:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
-function decimalToBinary(num) {
-  //Write you code here
-  
+function decimalToBinary(decimalNumber) {
+    if (decimalNumber === 0) {
+        return '0';
+    }
+
+    let binary = '';
+    while (decimalNumber > 0) {
+        const remainder = decimalNumber % 2;
+        binary = remainder + binary;
+        decimalNumber = Math.floor(decimalNumber / 2);
+    }
+
+    return binary;
 }
 
-window.decimalToBinary = decimalToBinary;
+// Example usage:
+const input1 = 7;
+console.log(`Input: ${input1}\nOutput: ${decimalToBinary(input1)}`);
+
+const input2 = 10;
+console.log(`Input: ${input2}\nOutput: ${decimalToBinary(input2)}`);
+
+const input3 = 33;
+console.log(`Input: ${input3}\nOutput: ${decimalToBinary(input3)}`);
